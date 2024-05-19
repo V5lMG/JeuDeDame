99fee27b1c2fc0516d09ee02a544b7ef966814ef
V5lMG <114313700+V5lMG@users.noreply.github.com>
2024-05-19 17:45:33 +0200

    Ajout FXML

--- src/main/java/iut/sae/jeudedame/Jeu.java
+++ src/main/java/iut/sae/jeudedame/Jeu.java
@@ -1,32 +1,23 @@
 package iut.sae.jeudedame;
 
-import iut.sae.jeudedame.objets.Partie;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Jeu extends Application {
-    private Partie partie; // Instance de la partie de jeu de dames
 
     @Override
     public void start(Stage stage) throws IOException {
         // Chargement de l'interface utilisateur depuis le fichier FXML
-        FXMLLoader fxmlLoader = new FXMLLoader(Jeu.class.getResource("jeu-dame.fxml"));
-        GridPane root = fxmlLoader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("jeu-dame.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
-        // Création de la scène et affichage de l'interface utilisateur
-        Scene scene = new Scene(root, 800, 600);
+        // Configuration de la fenêtre principale
         stage.setTitle("Jeu de Dames");
         stage.setScene(scene);
         stage.show();
-
-        // Initialisation de la partie de jeu de dames
-        partie = new Partie();
-        partie.initialisation();
     }
 
     public static void main(String[] args) {
