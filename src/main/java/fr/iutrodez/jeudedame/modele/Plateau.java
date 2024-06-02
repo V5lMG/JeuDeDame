32221873d340372684172b407b277e71238ac4bc
V5lMG <114313700+V5lMG@users.noreply.github.com>
2024-06-02 22:18:22 +0200

    Modification du plateau de jeu

---
+++ src/main/java/fr/iutrodez/jeudedame/modele/Plateau.java
@@ -0,0 +1,18 @@
+package fr.iutrodez.jeudedame.modele.objets;
+
+public class Plateau {
+    private String theme;
+
+    public Plateau() {
+        this.theme = "Classique";
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+}
+
